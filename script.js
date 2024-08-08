df2d8a4a46658d6e1a71695de033c9293c3aa80b
liver-juice <113276155+liver-juice@users.noreply.github.com>
2024-08-08 06:30:37 -0500

    generateBoard() creates a grid of tile objects. getBoard() returns the grid

--- script.js
+++ script.js
@@ -1,31 +1,39 @@
 function Gameboard(){
     //the board object itself
+    let board = [];
     
     const generateBoard = (rows, cols, mines) => {
-        // reference the board object itself.
-        let boardcontainer = document.querySelector('.boardcontainer');
-        //create a grid of the given rows and cols.
-        // each slot in the grid needs to be a Tile() instance but also an html div..
+        //create a grid/board of Tile() instances, with given rows and cols.
         for (let i = 0; i < rows; i++){
             //create a new row. 
-            let newRow = document.createElement('div');
-            newRow.classList.add('row');
+            let newRow = [];
             //then we fill that mf
             for (let j = 0; j < cols; j++){
-                let newTile = document.createElement('div');
-                newTile.classList.add('tile')
-                newRow.appendChild(newTile);
+                let newTile = Tile();
+                newRow.push(newTile);
             }
-            boardcontainer.appendChild(newRow);
+            board.push(newRow);
+            
         };
-        // we now have a bunch of rows, containing tile nodes, stored in boardcontainer.
+        // we now have a bunch of rows, containing tile instances,
+        // stored in board array.
     };
 
-    return{generateBoard};
+    const getBoard = () => {
+        //returns the board.
+        return board;
+    }
+
+    return{generateBoard, getBoard};
 }
 
 function Tile(){
     //each tile within the Gameboard is its own instantiation.
+    let isMine = false;
+    let isHidden = true; // tile isnt uncovered
+    let isFlagged = false;
+
+    return {};
 }
 
 function GameController(){
@@ -38,12 +46,13 @@ function GameController(){
     // let rows = document.querySelector('.rowsinput').innerHTML;
     // let cols = document.querySelector('.colsinput').innerHTML;
     // let mines = document.querySelector('.minesinput').innerHTML;
-
     let rows = 10;
     let cols = 10;
     let mines = 20;
     //create the board itself.
     board.generateBoard(rows, cols, mines)
+    console.log(board.getBoard());
+
 }
 
 game = GameController();
